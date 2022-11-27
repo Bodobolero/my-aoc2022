c9adadff5df7cc257ca7a35edbfeb5a4cfb48f9e
Peter Bendel <peter@familie-bendel.de>
2022-11-27 16:53:27 +0100

    Project scelection for Advent of code 2022

---
+++ src/day05.rs
@@ -0,0 +1,44 @@
+#![feature(test)]
+
+use itertools::Itertools;
+
+extern crate test;
+
+const INPUT: &str = include_str!("../inputs/input05.txt");
+
+fn part1() -> u32 {
+    0
+}
+
+fn part2() -> u32 {
+    0
+}
+
+pub fn main() {
+    println!("Part 1: Answer {}", part1());
+    println!("Part 2: Answer {} ", part2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(part1(), 0);
+    }
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2(), 0);
+    }
+    #[bench]
+    fn part1_bench(b: &mut Bencher) {
+        b.iter(part1);
+    }
+
+    #[bench]
+    fn part2_bench(b: &mut Bencher) {
+        b.iter(part2);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use my_aoc2020::{day01, day02, day03, day04, day05, day06, day07, day08};
+fn main() {
+    let solutions = [
+        day01::main,
+        day02::main,
+        day03::main,
+        day04::main,
+        day05::main,
+        day06::main,
+        day07::main,
+        day08::main,
+    ];
+    for (day, solution) in solutions.iter().enumerate() {
+        println!("------ Day {} ------", day + 1);
+        solution()
+    }
+}
