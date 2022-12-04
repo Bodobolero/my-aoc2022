e52667be737a3c102ed5034c5d5d912e04262fc4
Peter Bendel <peter@familie-bendel.de>
2022-12-04 08:42:56 +0100

    solution day4

--- src/day04.rs
+++ src/day04.rs
@@ -2,14 +2,70 @@
 
 extern crate test;
 
-const _INPUT: &str = include_str!("../inputs/input04.txt");
+use regex::Regex;
 
-fn part1() -> u32 {
-    0
+lazy_static::lazy_static! {
+    static ref RE_RULE: Regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
 }
 
-fn part2() -> u32 {
-    0
+const INPUT: &str = include_str!("../inputs/input04.txt");
+
+fn contains(a1: usize, a2: usize, b1: usize, b2: usize) -> bool {
+    let i1 = a1..=a2;
+    let i2 = b1..=b2;
+    for b in i2 {
+        if !i1.contains(&b) {
+            //println!("{}-{} does not contain {}-{}", a1, a2, b1, b2);
+            return false;
+        }
+    }
+    //println!("{}-{} contains {}-{}", a1, a2, b1, b2);
+    true
+}
+
+fn overlaps(a1: usize, a2: usize, b1: usize, b2: usize) -> bool {
+    let i1 = a1..=a2;
+    let i2 = b1..=b2;
+    for b in i2 {
+        if i1.contains(&b) {
+            // println!("{}-{} does overlap {}-{}", a1, a2, b1, b2);
+            return true;
+        }
+    }
+    // println!("{}-{} does not overlap {}-{}", a1, a2, b1, b2);
+    false
+}
+
+fn part1() -> usize {
+    INPUT
+        .lines()
+        .filter(|line| {
+            let caps = RE_RULE.captures(line).unwrap();
+            let (a1, a2, b1, b2) = (
+                caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(3).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(4).unwrap().as_str().parse::<usize>().unwrap(),
+            );
+            contains(a1, a2, b1, b2) || contains(b1, b2, a1, a2)
+        })
+        .count()
+}
+
+fn part2() -> usize {
+    INPUT
+        .lines()
+        .filter(|line| {
+            let caps = RE_RULE.captures(line).unwrap();
+            let (a1, a2, b1, b2) = (
+                caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(3).unwrap().as_str().parse::<usize>().unwrap(),
+                caps.get(4).unwrap().as_str().parse::<usize>().unwrap(),
+            );
+            overlaps(a1, a2, b1, b2) || overlaps(b1, b2, a1, a2)
+        })
+        .count()
 }
 
 pub fn main() {
@@ -24,11 +80,11 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 567);
     }
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 907);
     }
     #[bench]
     fn part1_bench(b: &mut Bencher) {
