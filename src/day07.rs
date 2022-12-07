7564681d9988eb6a1f66ecfb8a35d28e580c8045
Peter Bendel <peter@familie-bendel.de>
2022-12-07 11:15:23 +0100

    day07

--- src/day07.rs
+++ src/day07.rs
@@ -1,15 +1,128 @@
 #![feature(test)]
 
+use itertools::Itertools;
+
 extern crate test;
 
-const _INPUT: &str = include_str!("../inputs/input07.txt");
+const INPUT: &str = include_str!("../inputs/input07.txt");
 
-fn part1() -> u32 {
-    0
+const ALLSPACE: usize = 70000000;
+const NEEDEDSPACE: usize = 30000000;
+
+// since rust doesn't easily allow tree preserving parent and child references
+// we instead store all dirs in a vec and just the vec indexes in the parent child members of the struct
+#[derive(Debug)]
+struct Dir {
+    index: usize,
+    sum_offiles_size: usize, // initially flat size, in the end deep size
+    sub_dirs: Vec<usize>,
+    parent_dir: Option<usize>,
 }
 
-fn part2() -> u32 {
-    0
+// count sum of file size in a directory
+fn count_sizes(current_dir: &mut Dir, command: &str) {
+    current_dir.sum_offiles_size = command
+        .lines()
+        .skip(1)
+        .filter(|line| line.split(' ').next().unwrap() != "dir")
+        .map(|line| line.split(' ').next().unwrap().parse::<usize>().unwrap())
+        .sum();
+}
+
+// recursive function that adds child directory sizes to directory size
+fn add_child_size(dirs: &mut Vec<Dir>, i: usize) -> usize {
+    let children = dirs[i].sub_dirs.clone();
+    let recursive_childsize = children
+        .into_iter()
+        .map(|c| add_child_size(dirs, c))
+        .sum::<usize>();
+    dirs[i].sum_offiles_size += recursive_childsize;
+    dirs[i].sum_offiles_size
+}
+
+fn part1() -> usize {
+    let mut alldirs: Vec<Dir> = Vec::new();
+    let root = Dir {
+        index: alldirs.len(),
+        sum_offiles_size: 0,
+        sub_dirs: vec![],
+        parent_dir: None,
+    };
+    alldirs.push(root);
+    let mut current_dir = 0usize;
+    let commands = INPUT.split('$').skip(1);
+    for command in commands {
+        // println!("Parsing command: \n{}", command);
+        match command.lines().next().unwrap() {
+            " cd /" => current_dir = 0usize,
+            " ls" => count_sizes(&mut alldirs[current_dir], command),
+            " cd .." => current_dir = alldirs[current_dir].parent_dir.unwrap(),
+            // cd <x>
+            _ => {
+                let index = alldirs.len();
+                let subdir = Dir {
+                    index,
+                    sum_offiles_size: 0,
+                    sub_dirs: Vec::new(),
+                    parent_dir: Some(current_dir),
+                };
+                alldirs[current_dir].sub_dirs.push(index);
+                current_dir = subdir.index;
+                alldirs.push(subdir);
+            }
+        }
+    }
+    // println!("{:?}", alldirs);
+    add_child_size(&mut alldirs, 0);
+    alldirs
+        .into_iter()
+        .filter(|dir| dir.sum_offiles_size <= 100000)
+        .map(|dir| dir.sum_offiles_size)
+        .sum()
+}
+
+fn part2() -> usize {
+    let mut alldirs: Vec<Dir> = Vec::new();
+    let root = Dir {
+        index: alldirs.len(),
+        sum_offiles_size: 0,
+        sub_dirs: vec![],
+        parent_dir: None,
+    };
+    alldirs.push(root);
+    let mut current_dir = 0usize;
+    let commands = INPUT.split('$').skip(1);
+    for command in commands {
+        // println!("Parsing command: \n{}", command);
+        match command.lines().next().unwrap() {
+            " cd /" => current_dir = 0usize,
+            " ls" => count_sizes(&mut alldirs[current_dir], command),
+            " cd .." => current_dir = alldirs[current_dir].parent_dir.unwrap(),
+            // cd <x>
+            _ => {
+                let index = alldirs.len();
+                let subdir = Dir {
+                    index,
+                    sum_offiles_size: 0,
+                    sub_dirs: Vec::new(),
+                    parent_dir: Some(current_dir),
+                };
+                alldirs[current_dir].sub_dirs.push(index);
+                current_dir = subdir.index;
+                alldirs.push(subdir);
+            }
+        }
+    }
+    // println!("{:?}", alldirs);
+    add_child_size(&mut alldirs, 0);
+    let unused_space = ALLSPACE - alldirs[0].sum_offiles_size;
+    let to_delete = NEEDEDSPACE - unused_space;
+    alldirs
+        .into_iter()
+        .map(|dir| dir.sum_offiles_size)
+        .sorted()
+        .find(|dirsize| *dirsize > to_delete)
+        .unwrap()
 }
 
 pub fn main() {
@@ -24,11 +137,11 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 1306611);
     }
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 13210366);
     }
     #[bench]
     fn part1_bench(b: &mut Bencher) {
