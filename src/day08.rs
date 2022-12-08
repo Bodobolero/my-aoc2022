397e76601a67d3481cba1d161677c2787ec78c94
Peter Bendel <peter@familie-bendel.de>
2022-12-08 12:26:14 +0100

    solve day8

--- src/day08.rs
+++ src/day08.rs
@@ -1,93 +1,148 @@
 #![feature(test)]
 
 extern crate test;
-const _INPUT: &str = include_str!("../inputs/input08.txt");
+const INPUT: &str = include_str!("../inputs/input08.txt");
 
-#[derive(Debug)]
-struct Node<T> {
-    data: T,
-    children: Vec<Node<T>>,
-}
+fn part1() -> usize {
+    let lines: Vec<&[u8]> = INPUT.lines().map(|line| line.as_bytes()).collect();
+    let (c_lines, c_columns) = (lines.len(), lines[0].len());
+    let mut visible = vec![vec![false; c_columns]; c_lines];
+    // make edges visible
+    for i in 0..c_columns {
+        visible[0][i] = true;
+        visible[c_lines - 1][i] = true;
+    }
+    for i in 0..c_lines {
+        visible[i][0] = true;
+        visible[i][c_columns - 1] = true;
+    }
+    // println!("S {:?}", visible);
 
-#[derive(Debug)]
-struct NodeZipper<T> {
-    node: Node<T>,
-    parent: Option<Box<NodeZipper<T>>>,
-    index_in_parent: usize,
-}
-
-impl<T> NodeZipper<T> {
-    fn child(mut self, index: usize) -> NodeZipper<T> {
-        // Remove the specified child from the node's children.
-        // A NodeZipper shouldn't let its users inspect its parent,
-        // since we mutate the parents
-        // to move the focused nodes out of their list of children.
-        // We use swap_remove() for efficiency.
-        let child = self.node.children.swap_remove(index);
-
-        // Return a new NodeZipper focused on the specified child.
-        NodeZipper {
-            node: child,
-            parent: Some(Box::new(self)),
-            index_in_parent: index,
+    // mark left to right
+    for i in 1..c_lines - 1 {
+        for j in 1..c_columns - 1 {
+            let mut yes = true;
+            for k in 0..j {
+                if lines[i][j] <= lines[i][k] {
+                    yes = false;
+                    break;
+                }
+            }
+            if yes {
+                visible[i][j] = true;
+            }
         }
     }
-
-    fn parent(self) -> NodeZipper<T> {
-        // Destructure this NodeZipper
-        let NodeZipper {
-            node,
-            parent,
-            index_in_parent,
-        } = self;
-
-        // Destructure the parent NodeZipper
-        let NodeZipper {
-            node: mut parent_node,
-            parent: parent_parent,
-            index_in_parent: parent_index_in_parent,
-        } = *parent.unwrap();
-
-        // Insert the node of this NodeZipper back in its parent.
-        // Since we used swap_remove() to remove the child,
-        // we need to do the opposite of that.
-        parent_node.children.push(node);
-        let len = parent_node.children.len();
-        parent_node.children.swap(index_in_parent, len - 1);
-
-        // Return a new NodeZipper focused on the parent.
-        NodeZipper {
-            node: parent_node,
-            parent: parent_parent,
-            index_in_parent: parent_index_in_parent,
+    // println!("L2R {:?}", visible);
+    // mark right to left
+    for i in 1..c_lines - 1 {
+        for j in (1..c_columns - 1).rev() {
+            let mut yes = true;
+            for k in j + 1..c_columns {
+                if lines[i][j] <= lines[i][k] {
+                    yes = false;
+                    break;
+                }
+            }
+            if yes {
+                visible[i][j] = true;
+            }
         }
     }
-
-    fn finish(mut self) -> Node<T> {
-        while let Some(_) = self.parent {
-            self = self.parent();
-        }
-
-        self.node
-    }
-}
-
-impl<T> Node<T> {
-    fn zipper(self) -> NodeZipper<T> {
-        NodeZipper {
-            node: self,
-            parent: None,
-            index_in_parent: 0,
+    // println!("R2L {:?}", visible);
+    // mark top to botton
+    for j in 1..c_columns - 1 {
+        for i in 1..c_lines - 1 {
+            let mut yes = true;
+            for k in 0..i {
+                if lines[i][j] <= lines[k][j] {
+                    yes = false;
+                    break;
+                }
+            }
+            if yes {
+                visible[i][j] = true;
+            }
         }
     }
+    // println!("T2B {:?}", visible);
+
+    // mark botton to top
+    for j in 1..c_columns - 1 {
+        for i in (1..c_lines - 1).rev() {
+            let mut yes = true;
+            for k in i + 1..c_lines {
+                if lines[i][j] <= lines[k][j] {
+                    yes = false;
+                    break;
+                }
+            }
+            if yes {
+                visible[i][j] = true;
+            }
+        }
+    }
+    // println!("B2T{:?}", visible);
+
+    // count visible trees
+    visible
+        .into_iter()
+        .map(|line| line.into_iter().map(|b| b as usize).sum::<usize>())
+        .sum::<usize>()
 }
 
-fn part1() -> u32 {
-    0
-}
+fn part2() -> usize {
+    let lines: Vec<&[u8]> = INPUT.lines().map(|line| line.as_bytes()).collect();
+    let (c_lines, c_columns) = (lines.len(), lines[0].len());
+    let mut dist = vec![vec![1usize; c_columns]; c_lines];
+    for i in 1..c_lines - 1 {
+        for j in 1..c_columns - 1 {
+            //left
+            if j > 1 {
+                let mut k = j - 1;
+                while k > 0 && lines[i][k] < lines[i][j] {
+                    k -= 1;
+                }
+                dist[i][j] *= j - k;
+            }
+            // println!("[{}][{}]L:{}", i, j, dist[i][j]);
 
-fn part2() -> u32 {
-    0
+            //right
+            if j < c_columns - 1 {
+                let mut k = j + 1;
+                while k < c_columns - 1 && lines[i][k] < lines[i][j] {
+                    k += 1;
+                }
+                dist[i][j] *= k - j;
+            }
+            // println!("[{}][{}]R:{}", i, j, dist[i][j]);
+
+            // up
+            if i > 1 {
+                let mut k = i - 1;
+                while k > 0 && lines[k][j] < lines[i][j] {
+                    k -= 1;
+                }
+                dist[i][j] *= i - k;
+            }
+            // println!("[{}][{}]U:{}", i, j, dist[i][j]);
+
+            // down
+            if i < c_lines - 1 {
+                let mut k = i + 1;
+                while k < c_lines - 1 && lines[k][j] < lines[i][j] {
+                    k += 1;
+                }
+                dist[i][j] *= k - i;
+            }
+            // println!("[{}][{}]D:{}", i, j, dist[i][j]);
+        }
+    }
+    // println!("{:?}", dist);
+    dist.into_iter()
+        .map(|line| line.into_iter().max().unwrap())
+        .max()
+        .unwrap()
 }
 
 pub fn main() {
@@ -102,11 +157,11 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 1825);
     }
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 235200);
     }
     #[bench]
     fn part1_bench(b: &mut Bencher) {
