91be86536970fa09e896faf21bbae000537354c5
Peter Bendel <peter@familie-bendel.de>
2022-12-16 17:12:10 +0100

    day16 part2

--- src/day16.rs
+++ src/day16.rs
@@ -93,9 +93,130 @@ fn step<'a>(
     }
 }
 
+fn step2<'a>(
+    minutes: usize,
+    ppm: usize,
+    pt: usize,
+    current1: &'a str,
+    current2: &'a str,
+    valves: &'a HashMap<&str, Valve>,
+    open_valves: HashSet<&str>,
+    result: &mut usize,
+    best_path: &mut HashMap<(&'a str, &'a str, usize), usize>,
+) {
+    if minutes == 27 {
+        if pt > *result {
+            *result = pt;
+            // println!(
+            //     "done c1 {} c2 {} ppm {} pt {} open valves {:?}",
+            //     current1, current2, ppm, pt, open_valves
+            // );
+        }
+    } else {
+        let new_pt = pt + ppm;
+        let best = best_path.get(&(current1, current2, minutes));
+        let best2 = best_path.get(&(current2, current1, minutes));
+        if (best.is_some() && *best.unwrap() >= new_pt)
+            || (best2.is_some() && *best2.unwrap() >= new_pt)
+        {
+            return;
+        }
+        // println!(
+        //     "min {} c1 {} c2 {} ppm {} pt {} open valves {:?}",
+        //     minutes, current1, current2, ppm, pt, open_valves
+        // );
+        let entry = best_path
+            .entry((current1, current2, minutes))
+            .or_insert(new_pt);
+        *entry = new_pt;
+
+        let valve1 = valves.get(current1).unwrap();
+        let valve2 = valves.get(current2).unwrap();
+        if valve1.name != valve2.name
+            && valve1.f > 0
+            && valve2.f > 0
+            && !open_valves.contains(current1)
+            && !open_valves.contains(current2)
+        {
+            // if both valves are still closed
+            // open both valves
+            let mut open_valves_new = open_valves.clone();
+            open_valves_new.insert(current1);
+            open_valves_new.insert(current2);
+            let new_ppm = ppm + valve1.f + valve2.f;
+            step2(
+                minutes + 1,
+                new_ppm,
+                new_pt,
+                current1,
+                current2,
+                valves,
+                open_valves_new,
+                result,
+                best_path,
+            );
+        }
+        if valve1.f > 0 && !open_valves.contains(current1) {
+            // if valve1 is still closed
+            // open it
+            let mut open_valves_new = open_valves.clone();
+            open_valves_new.insert(current1);
+            let new_ppm = ppm + valve1.f;
+            for target2 in &valve2.target_valves {
+                step2(
+                    minutes + 1,
+                    new_ppm,
+                    new_pt,
+                    current1,
+                    target2,
+                    valves,
+                    open_valves_new.clone(),
+                    result,
+                    best_path,
+                );
+            }
+        }
+        if valve2.f > 0 && !open_valves.contains(current2) {
+            // if valve2 is still closed
+            // open it
+            let mut open_valves_new = open_valves.clone();
+            open_valves_new.insert(current2);
+            let new_ppm = ppm + valve2.f;
+            for target1 in &valve1.target_valves {
+                step2(
+                    minutes + 1,
+                    new_ppm,
+                    new_pt,
+                    target1,
+                    current2,
+                    valves,
+                    open_valves_new.clone(),
+                    result,
+                    best_path,
+                );
+            }
+        }
+        for target1 in &valve1.target_valves {
+            for target2 in &valve2.target_valves {
+                step2(
+                    minutes + 1,
+                    ppm,
+                    new_pt,
+                    target1,
+                    target2,
+                    valves,
+                    open_valves.clone(),
+                    result,
+                    best_path,
+                );
+            }
+        }
+    }
+}
+
 fn part1() -> usize {
     let valves = parse_valves(INPUT);
-    println!("{:?}", valves);
+    // println!("{:?}", valves);
 
     let pressure_per_minute: usize = 0;
     let pressure_total: usize = 0;
@@ -117,7 +238,27 @@ fn part1() -> usize {
 }
 
 fn part2() -> usize {
-    0
+    let valves = parse_valves(INPUT);
+    // println!("{:?}", valves);
+
+    let pressure_per_minute: usize = 0;
+    let pressure_total: usize = 0;
+    let open_valves: HashSet<&str> = HashSet::new();
+    let mut result: usize = 0;
+    let mut best_path: HashMap<(&str, &str, usize), usize> = HashMap::new();
+    step2(
+        1,
+        pressure_per_minute,
+        pressure_total,
+        "AA",
+        "AA",
+        &valves,
+        open_valves.clone(),
+        &mut result,
+        &mut best_path,
+    );
+
+    result
 }
 
 pub fn main() {
@@ -132,11 +273,11 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 1651);
+        assert_eq!(part1(), 1559);
     }
     #[test]
     fn part2_test() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 2191);
     }
     #[bench]
     fn part1_bench(b: &mut Bencher) {
