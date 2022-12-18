0267ddfb5fec30de6ca438a0a65f47d269477b4c
Peter Bendel <peter@familie-bendel.de>
2022-12-18 15:42:56 +0100

    dead end for part2

--- src/day18.rs
+++ src/day18.rs
@@ -1,20 +1,20 @@
 #![feature(test)]
 
-use std::num;
+use std::collections::HashSet;
 
 extern crate test;
 const INPUT: &str = include_str!("../inputs/input18.txt");
 
-fn scan_cubes() -> Vec<(i8, i8, i8)> {
+fn scan_cubes() -> Vec<[i8; 3]> {
     INPUT
         .lines()
         .map(|line| {
             let mut num_iterator = line.split(',').map(|s| s.parse::<i8>().unwrap());
-            (
+            [
                 num_iterator.next().unwrap(),
                 num_iterator.next().unwrap(),
                 num_iterator.next().unwrap(),
-            )
+            ]
         })
         .collect()
 }
@@ -25,7 +25,7 @@ fn part1() -> usize {
     for c1 in &cubes {
         let mut c1_free = 6;
         for c2 in &cubes {
-            let distance = (c1.0 - c2.0).abs() + (c1.1 - c2.1).abs() + (c1.2 - c2.2).abs();
+            let distance = (c1[0] - c2[0]).abs() + (c1[1] - c2[1]).abs() + (c1[2] - c2[2]).abs();
             if distance == 1 {
                 c1_free -= 1;
             }
@@ -35,8 +35,87 @@ fn part1() -> usize {
     free_sides
 }
 
+fn traverse(
+    cube: usize,
+    cubes: &Vec<[i8; 3]>,
+    reached: &mut Vec<bool>,
+    sides: &Vec<Vec<Option<usize>>>,
+    steam: &mut HashSet<[i8; 3]>,
+) -> usize {
+    if reached[cube] {
+        return 0;
+    };
+    let mut free_sides: usize = 0;
+    reached[cube] = true;
+    let mut cube_sides: usize = 6;
+    for k in 0..6 {
+        let side = sides[cube][k];
+        if side.is_none() {
+            // check if side reachable by steam
+            let mut space = cubes[cube];
+            let val = if k > 3 { 1 } else { -1 };
+            let index = k % 3;
+            space[index] += val;
+            if !steam.contains(&space) {}
+        }
+    }
+    for k in 0..6 {
+        let side = sides[cube][k];
+        if side.is_some() {
+            cube_sides -= 1;
+            if (!reached[side.unwrap()]) {
+                free_sides += traverse(side.unwrap(), cubes, reached, sides, steam);
+            }
+        } else {
+            // check if side reachable by steam
+        }
+    }
+    free_sides + cube_sides
+}
+
 fn part2() -> usize {
-    0
+    let cubes = scan_cubes();
+    // sides: -x, +x, -y, +y, -z, +z
+    let mut sides: Vec<Vec<Option<usize>>> = vec![vec![None; 6]; cubes.len()];
+    // build grid
+    for (i, c1) in cubes.iter().enumerate() {
+        for (j, c2) in cubes.iter().enumerate() {
+            let distance = (c1[0] - c2[0]).abs() + (c1[1] - c2[1]).abs() + (c1[2] - c2[2]).abs();
+            if distance == 1 {
+                for k in 0..3 {
+                    if c1[k] < c2[k] {
+                        sides[i][k * 2] = Some(j);
+                    }
+                    if c1[k] > c2[k] {
+                        sides[i][k * 2 + 1] = Some(j);
+                    }
+                }
+            }
+        }
+    }
+    let mut reached: Vec<bool> = vec![false; cubes.len()];
+    let mut free_sides: usize = 0;
+    // collect free sides reachable from the outside
+    while reached.iter().filter(|b| true).count() < reached.len() {
+        let mut steam: HashSet<[i8; 3]> = HashSet::new();
+        // find a cube at the outside of connected graph with minimum x
+        let minx = cubes
+            .iter()
+            .enumerate()
+            .filter(|&(i, c)| !reached[i])
+            .map(|(i, c)| c[0])
+            .min()
+            .unwrap();
+
+        let (ci, c) = cubes
+            .iter()
+            .enumerate()
+            .find(|&(i, c)| c[0] == minx)
+            .unwrap();
+        steam.insert([minx - 1, c[1], c[2]]);
+        free_sides += traverse(ci, &cubes, &mut reached, &mut sides, &mut steam);
+    }
+    free_sides
 }
 
 pub fn main() {
@@ -51,7 +130,7 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(), 64);
+        assert_eq!(part1(), 3564);
     }
     #[test]
     fn part2_test() {
